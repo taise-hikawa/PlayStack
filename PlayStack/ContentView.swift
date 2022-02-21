cd1bcc3d5a55583b427cb96489e4d2f3e9229d89
taise-hikawa <cmmkarubi@gmail.com>
2022-02-21 12:58:48 +0900

    Initial Commit

---
+++ PlayStack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayStack
+//
+//  Created by 樋川大聖 on 2022/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayStack/PlayStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayStackApp.swift
+//  PlayStack
+//
+//  Created by 樋川大聖 on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PlayStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
