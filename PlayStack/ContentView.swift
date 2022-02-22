26ea7b537f4f0c193a43e1824ad967c9974a40b0
taise-hikawa <cmmkarubi@gmail.com>
2022-02-22 18:21:23 +0900

    test LazyVStack

--- PlayStack/ContentView.swift
+++ PlayStack/ContentView.swift
@@ -9,8 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                VStack {
+                    ForEach(0..<10, id: \.self) { i in
+                        Text("Section1-\(i)")  // ここでブレークポイントを貼ると毎回停止する。
+                            .frame(height: 50)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.blue.opacity(0.2))
+                }
+                VStack {
+                    ForEach(0..<1) { i in
+                        Text("jjjj")
+                    }
+                }
+
+                VStack {
+                    ForEach(10..<20, id: \.self) { i in
+                        Text("Section2-\(i)")
+                            .frame(height: 50)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.red.opacity(0.2))
+                }
+
+                VStack {
+                    ForEach(20..<30, id: \.self) { i in
+                        Text("Section3-\(i)")
+                            .frame(height: 50)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.green.opacity(0.2))
+                }
+
+                VStack {
+                    ForEach(30..<39, id: \.self) { i in
+                        Text("Section4-\(i)")
+                            .frame(height: 50)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.yellow.opacity(0.2))
+                }
+            }
+        }
     }
 }
 
